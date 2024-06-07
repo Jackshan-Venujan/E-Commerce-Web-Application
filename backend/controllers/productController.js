2ed14f0229506d686904e33d564f4c911cf7d1dd
venujan <81431056+Jackshan-Venujan@users.noreply.github.com>
2024-06-07 23:04:23 +0530

    All three API finished

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,9 @@
 const ProductModel = require('../models/productModel');
 
+//Get Products API - /api/v1/product
 exports.getProducts = async(req,res,next) => {
     // to retrive all data leave empty else have ti be specific enter name
-    const products = await ProductModel.find({name:'something'});  
+    const products = await ProductModel.find({});  
 
 
     res.json({
@@ -11,9 +12,23 @@ exports.getProducts = async(req,res,next) => {
     })
 }
 
-exports.getSingleProduct = (req,res,next) => {
-    res.json({
-        success: true,
-        message: 'Get single products working!'
+
+//Get Single Products API - /api/v1/product/:id
+exports.getSingleProduct = async(req,res,next) => {
+    try{
+        //console.log(req.params.id,'ID')
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
     })
+
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: 'Unable to find the product, Check the product ID'                       //error.message
+    })
+    }
+    
+    
 }
